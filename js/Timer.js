e411bcde2ebb0377f43158601fe2b4cc7697049a
alexandrerussi <alexandrerussijunior@gmail.com>
2023-10-18 16:09:38 -0300

    ajustes para executar na next sem internet

--- js/Timer.js
+++ js/Timer.js
@@ -49,10 +49,10 @@ export default class Timer {
             this.start();
         });
 
-        this.el.pause.addEventListener("click", () => {
-            togglePlay();
-            this.stop();
-        });
+        // this.el.pause.addEventListener("click", () => {
+        //     togglePlay();
+        //     this.stop();
+        // });
 
         this.el.stop.addEventListener("click", () => {
             resetMsc();
@@ -96,9 +96,18 @@ export default class Timer {
 
     static getHTML() {
         return `
-            <button id="btn-pause"><i class="fa fa-pause"></i></button>
-            <button id="btn-play"><i class="fa fa-play"></i></button>
-            <button id="btn-stop"><i class="fa fa-stop"></i></button>
+            <!--<button id="btn-pause">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
+            </button>-->
+            
+            <button id="btn-play">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
+            </button>
+
+            <button id="btn-stop">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https:// fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 128C0 92.7 28.7 64 64 64H320c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/></svg>
+            </button>
+
             <!-- <button id="btn-reset"><i class="fa fa-solid fa-reply"></i></button> -->
 
             <div class="clock row align-content-center ">
@@ -114,16 +123,16 @@ export default class Timer {
             <div class="text-center">
                 <form action="">
                     <div>
-                        <input type="text" name="nome" id="nome" placeholder="Nome do jogador">
+                        <input type="text" name="nome" id="nome" placeholder="Nome do jogador" autocomplete="off">
                     </div>
                     <div>
-                        <input type="text" name="wpp" id="wpp" placeholder="WhatsApp">
+                        <input type="text" name="wpp" id="wpp" placeholder="WhatsApp" autocomplete="off">
                     </div>
                     <div>
-                        <input type="number" name="qtd" id="qtd" placeholder="EcoPoints">
+                        <input type="number" name="qtd" id="qtd" placeholder="EcoPoints" autocomplete="off">
                     </div>
                     <div>
-                        <button type="submit" id="btn-save"><i class="fa fa-save"></i> &nbsp;SALVAR</button>
+                        <button type="submit" id="btn-save">SALVAR</button>
                     </div>
                 </form>
             </div>
